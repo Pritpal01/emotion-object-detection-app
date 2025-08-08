3988e4a3761b064e3b88252db9e4e967c102988d
Pritpal Singh <122661503+Pritpal01@users.noreply.github.com>
2025-08-08 21:03:03 +0530

    Update useless_database.py

--- useless_database.py
+++ useless_database.py
@@ -1,7 +1,7 @@
 
 from pymongo import MongoClient
 
-client = MongoClient("mongodb+srv://users:emotions@cluster1.xwnrllv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1")
+client = MongoClient("")
 db = client["emotion_app"]
 users_col = db["users"]
 emotions_col = db["emotions"]
